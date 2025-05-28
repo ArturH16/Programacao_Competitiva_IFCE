b01196e6f57be00bead7e057e190149793cfd0fd
ArturH16 <holandaartur163@gmail.com>
2025-05-28 19:05:09 -0300

    Update problema_1066.py
    O erro EOF foi resolvido e essa foi a versão aceita pelo beecrowd.

--- problema_1066.py
+++ problema_1066.py
@@ -1,13 +1,15 @@
-pares = positivos = negativos = impares = 0
-
+pares = positivos = negativos = impares = numeros = 0
 while True:
-    num = int(input('Digite um valor: '))
+    numeros += 1
+    num = int(input())
     if num % 2 == 0:
         pares += 1
     else:
         impares += 1
-    if num == abs(num):
+    if num > 0:
         positivos += 1
-    else:
+    elif num < 0:
         negativos += 1
-print(f'{pares} valor (es) par(es)\n{impares} valor (es) impar(es)\n{positivos} valor (es) positivo(s)\n{negativos} valor (es) negativo(s).')
+    if numeros == 5:
+        break
+print(f'{pares} valor(es) par(es)\n{impares} valor(es) impar(es)\n{positivos} valor(es) positivo(s)\n{negativos} valor(es) negativo(s)')
